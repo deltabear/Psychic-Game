0d2c8247eeebd7eaf4edd90b6df9eaaa76e6a40c
max delzer <emma.delzer@gmail.com>
2019-05-11 23:41:33 -0700

    Rearranged page and rewrote event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,48 @@
 //Variables to hold numbers
-var wins = 0; //Number of Letters correctly guessed
-var losses = 0; //Number of Incorrect guesses
-var guessesLeft = 13 //Number of Guesses Remaining: (# of guesses remaining for the user).
-var guessesSoFar = []; //Display letters which have been insput by user
+var options = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+var wins= 0;
+var losses= 0;
+var turns= 13;
+var guess="";
 
-//Display the following on the page:
-var directionsText = document.getElementById("directions-text");
 
-//Wins: (# of times user guessed the word correctly).
-var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById("computerchoice-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesLeft = document.getElementById("guesses-left");
-var guessesSoFar = document.getElementById("guesses-so-far");
-
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-
-//Choose random character
+var computerGuess;
+var intialized=true;
 document.onkeyup = function(event) {
+debugger
+if(intialized){
+computerGuess = options[Math.floor(Math.random()*options.length)];
+intialized=false;
+}
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    let computerGuess = computerChoices[Math.floor(Math.random()* letters.length)]; 
 
-    if ((userGuess === computerChoice)) {
-     wins++;
-    } else {
-     losses++; guessesLetters + userGuess
-    }
+var userguess = event.key;
+if (userguess == computerGuess) {
+computerGuess = options[Math.floor(Math.random()*options.length)];
+  intialized=true;
+  wins++;
+  turns = 9; 
+  guess = ""; 
+}
 
-    // Display the user and computer guesses, and wins/losses/ties.
-    userChoiceText.textContent = "You chose: " + userGuess;
-    computerChoiceText.textContent = "The computer chose: " + computerGuess;
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
-    tiesText.textContent = "ties: " + ties;
+else if(userguess !== computerGuess) {
+ turns--;
+ guess += userguess + ", ";}
 
-    function updateGuessesLeft() {
-        // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
-        document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
-    }
+  if (turns ==0) {
+    losses++;
+    turns= 9;
+    guess="";
 
-    function updateGuessesSoFar() {
-        // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
-    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-    }
+
+
+}
+
+var html = "<p>Guess what letter I'm thinking of</p>" + 
+  "<p>wins: " + wins + "</p>" +
+  "<p>losses: " + losses + "</p>" +
+  "<p>guesses left: " + turns + "</p>"+
+  "<p>Your Guess So Far: " + guess + "</p>";
+
+document.querySelector('#game').innerHTML = html;
 };
-
-//Browser Testing
-console.log(letters);
-console.log(computerGuess)
-
-// Display the user and computer guesses, and wins/losses/ties.
-guessesSoFar.textContent = "Letters guessed: " + letterGuesses;
-guessesLeft.textContent = 13 - losses;
